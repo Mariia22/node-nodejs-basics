9f632861e0093755379fe6968dec0ed5e8584472
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-03 16:45:10 +0200

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { access, writeFile, mkdir } from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const create = async () => {
-    // Write your code here 
+  const content = "I am fresh and young";
+  const folder = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "files",
+  );
+  const file = "fresh.txt";
+  const filePath = path.join(folder, file);
+
+  try {
+    await access(folder).catch(async () => await mkdir(folder));
+    await access(filePath)
+      .then(() => console.log("FS operation failed"))
+      .catch(async () => await writeFile(filePath, content));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await create();
