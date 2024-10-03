fc3a94a8bf72b6575e76aaf96ab4d4c60b1f2bcb
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-03 22:37:50 +0200

    feat: delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { ERRORMESSAGE, FOLDER } from "./const.js";
+import path from "path";
+import { access, unlink } from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  const fileForRemoving = path.join(FOLDER, "fileToRemove.txt");
+
+  try {
+    await access(fileForRemoving)
+      .then(async () => {
+        await unlink(fileForRemoving);
+      })
+      .catch(() => {
+        throw new Error(ERRORMESSAGE);
+      });
+  } catch (error) {
+    throw error;
+  }
 };
 
 await remove();
