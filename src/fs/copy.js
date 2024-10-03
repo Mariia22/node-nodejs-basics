03c200c3c58290efdf555867e8337318cb0df498
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-03 17:50:56 +0200

    feat: copy files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { access, mkdir, cp } from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  const pathToFolder = path.dirname(fileURLToPath(import.meta.url));
+  const folderSource = path.join(pathToFolder, "files");
+  const folderTarget = path.join(pathToFolder, "files_copy");
+  const errorMessage = "FS operation failed";
+
+  try {
+    await access(folderTarget)
+      .then(async () => {
+        throw new Error(errorMessage);
+      })
+      .catch(async () => {
+        await access(folderSource)
+          .then(async () => {
+            await mkdir(folderTarget);
+            await cp(folderSource, folderTarget, { recursive: true });
+          })
+          .catch(async () => {
+            throw new Error(errorMessage);
+          });
+      });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await copy();
