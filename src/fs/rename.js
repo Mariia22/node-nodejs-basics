c6e8314ec441e4c8eb3e6a06b06e2e8b35858a5e
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-03 22:25:54 +0200

    feat: rename file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,36 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { access, rename as renameFile } from "fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  const folder = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "files",
+  );
+  const fileSource = path.join(folder, "wrongFilename.txt");
+  const fileTarget = path.join(folder, "properFilename.md");
+  const errorMessage = "FS operation failed";
+  const errorNoEntry = "ENOENT";
+
+  try {
+    await access(fileSource).catch(() => {
+      throw new Error(errorMessage);
+    });
+
+    await access(fileTarget)
+      .then(async () => {
+        throw new Error(errorMessage);
+      })
+      .catch(async (error) => {
+        if (error.code === errorNoEntry) {
+          await renameFile(fileSource, fileTarget);
+        } else {
+          throw error;
+        }
+      });
+  } catch (error) {
+    throw error;
+  }
 };
 
 await rename();
