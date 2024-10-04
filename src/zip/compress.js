f5d88d7e1f806ad6de24469bb04f67aaf2670bc6
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-04 22:27:30 +0200

    feat: unzip file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,23 +1,16 @@
 import { createGzip } from "zlib";
 import { createReadStream, createWriteStream } from "fs";
 import { pipeline } from "stream";
-import path from "path";
-import { fileURLToPath } from "url";
 import { access } from "fs/promises";
+import { FILE, ZIP } from "./const.js";
 
 const compress = async () => {
-  const dirname = path.join(
-    path.dirname(fileURLToPath(import.meta.url)),
-    "files",
-  );
-  const fileForZip = path.join(dirname, "fileToCompress.txt");
-  const zip = path.join(dirname, "archive.gz");
   try {
-    await access(fileForZip);
+    await access(FILE);
     pipeline(
-      createReadStream(fileForZip),
+      createReadStream(FILE),
       createGzip(),
-      createWriteStream(zip),
+      createWriteStream(ZIP),
       (err) => {
         if (err) {
           console.log("Pipeline failed", err.message);
