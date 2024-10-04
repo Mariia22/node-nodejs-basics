bc035976ff8a02c0105b6eb0f8e01b00dd5692fe
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-04 11:30:00 +0200

    feat: parse environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+
+  const result = Object.keys(process.env)
+    .filter((key) => key.startsWith(prefix))
+    .map((key) => `${key}=${process.env[key]}`)
+    .join("; ");
+
+  result
+    ? console.log(result)
+    : console.log("There is no variables with prefix RSS_");
 };
 
 parseEnv();
