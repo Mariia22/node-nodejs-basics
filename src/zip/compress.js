097c3a7b44e7670a4d418568c01745d4cf353926
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-04 22:17:15 +0200

    feat: zip file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,32 @@
+import { createGzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import path from "path";
+import { fileURLToPath } from "url";
+import { access } from "fs/promises";
+
 const compress = async () => {
-    // Write your code here 
+  const dirname = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "files",
+  );
+  const fileForZip = path.join(dirname, "fileToCompress.txt");
+  const zip = path.join(dirname, "archive.gz");
+  try {
+    await access(fileForZip);
+    pipeline(
+      createReadStream(fileForZip),
+      createGzip(),
+      createWriteStream(zip),
+      (err) => {
+        if (err) {
+          console.log("Pipeline failed", err.message);
+        }
+      },
+    );
+  } catch (error) {
+    throw error;
+  }
 };
 
 await compress();
