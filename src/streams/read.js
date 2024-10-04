777e05b26768a07f86bb9afc87957408258c6c0d
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-04 14:44:49 +0200

    feat: add read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { access } from "fs/promises";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const file = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fileToRead.txt",
+  );
+  try {
+    await access(file);
+    const input = createReadStream(file);
+    input.pipe(process.stdout);
+    input.on("error", (err) => {
+      console.log("Error reading file:", err);
+    });
+  } catch (error) {
+    console.log(`There is no file ${error}`);
+  }
 };
 
 await read();
