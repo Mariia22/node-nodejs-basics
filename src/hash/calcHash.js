3379b1a1118db45e3f2afb1f166cc6b122318575
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-04 14:22:13 +0200

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fileToCalculateHashFor.txt",
+  );
+  const hash = createHash("sha256");
+  const input = createReadStream(file);
+
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(`${hash.digest("hex")}`);
+    }
+  });
 };
 
 await calculateHash();
